3e54997f92b3cf7cc0cba432afa1d0dffef3894f
Antonio Gonzalez <goantonio31@outlook.com>
2021-03-21 13:03:20 -0700

    added a service worker and the manifest

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Antonio-admin:passw0rd@cluster0.ahew4.mongodb.net/budgetDB?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+//update for heroku on deployment
 const PORT = 3000;
 
 const app = express();
@@ -15,13 +27,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/budgetDB", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
+//create connection to mongodb
+
 // routes
-app.use(require("./Develop/routes/api.js"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
